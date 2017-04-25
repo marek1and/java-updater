5a4f5660c700ceda216c2a7598ab7881b00cb788
Marek Andrzejewski <marek1and@gmail.com>
2017-04-25 21:23:02 +0200

    Updated script to download only newest java. Code updates to met PEP-8 guide

--- java-updater.py
+++ java-updater.py
@@ -1,44 +1,40 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
 import urllib
 import urllib2
-import os, sys
+import os
+import sys
 import tarfile
 import time
 import subprocess
 import re
 import collections
 from getopt import getopt, GetoptError
-from distutils.extension import Extension
-from symbol import except_clause
 
 """
     Oracle download pages with Java 6, 7 and 8
 """
-dPages = []
-dPages.append("http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html")
-dPages.append("http://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html")
-dPages.append("http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html")
-dPages.append("http://www.oracle.com/technetwork/java/javase/downloads/jre7-downloads-1880261.html")
-dPages.append("http://www.oracle.com/technetwork/java/javase/downloads/java-archive-javase8-2177648.html");
-dPages.append("http://www.oracle.com/technetwork/java/javase/downloads/java-archive-downloads-javase7-521261.html");
-dPages.append("http://www.oracle.com/technetwork/java/javase/downloads/java-archive-downloads-javase6-419409.html")
-dPages.append("http://www.oracle.com/technetwork/java/javase/downloads/jdk8-arm-downloads-2187472.html")
-dPages.append("http://www.oracle.com/technetwork/java/javase/downloads/jdk7-arm-downloads-2187468.html")
+f_name_pattern = "(jdk|jre)-([0-9]+[u0-9]*[-p]*)-([a-z]+)-([a-z0-9]+[-piftwrm]*)(-vfp-(hflt|sflt){1})*\.([a-zA-Z\.]+)"
+download_pages = [
+    "http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html"
+]
 
-def Tree():
-    return collections.defaultdict(Tree)
 
-def getPage(link):
-    print "Getting content from page:",link
+def object_tree():
+    return collections.defaultdict(object_tree)
+
+
+def get_page(link):
+    print "Getting content from page:", link
     page = urllib.urlopen(link)
     content = page.read()
     page.close()
     return content
 
+
 class DownloadLinksParser:
     
-    __versionsTree__ = Tree()
+    __versionsTree__ = object_tree()
     __pages__ = []
     
     def __init__(self, pages):
@@ -46,75 +42,78 @@ class DownloadLinksParser:
     
     def parse(self):
         for dPage in self.__pages__:
-            content = getPage(dPage)
+            content = get_page(dPage)
     
-            title= ""
-            for line in iter(content.splitlines()):
+            title = ""
+            for _line in iter(content.splitlines()):
                 
-                type = ""
-                system = ""
-                arch = ""
-                version = ""
+                _type = ""
+                _system = ""
+                _arch = ""
+                _version = ""
                 extension = ""
                 
-                dLine = re.search('downloads\[\'.*\'\]\[\'.*\'\].*', line)
-                if dLine:
-                    downloadLine = dLine.group(0)
+                d_line = re.search('downloads\[\'.*\'\]\[\'.*\'\].*', _line)
+                if d_line:
+                    download_line = d_line.group(0)
                     
-                    if "-demos-" in downloadLine or "-apidocs" in downloadLine:
+                    if "-demos-" in download_line or "-apidocs" in download_line:
                         continue
                     
-                    dictGroups = re.finditer('(\[\'([\-\.a-zA-Z0-9]*)\'\])', downloadLine)
-                    if dictGroups:
-                        dictType = ""
-                        fileName = ""
-                        
-                        fileDict = ""
+                    dict_groups = re.finditer('(\[\'([\-\.a-zA-Z0-9]*)\'\])', download_line)
+                    if dict_groups:
+                        dict_type = ""
+                        file_dict = {}
                         i = 0
-                        for dictGroup in dictGroups:
+                        for dict_group in dict_groups:
                             if i == 1:
-                                dictType = dictGroup.group(2)
+                                dict_type = dict_group.group(2)
                             elif i == 2:
-                                fileName = dictGroup.group(2)
+                                file_name = dict_group.group(2)
                                 
-                                fNameGroups = re.search('(jdk|jre)-([0-9]+[u0-9]*[-p]*)-([a-z]+)-([a-z0-9]+[-piftwrm]*)(-vfp-(hflt|sflt){1})*\.([a-zA-Z\.]+)', fileName)
+                                f_name_groups = re.search(f_name_pattern, file_name)
                                 
-                                if fNameGroups:
-                                    type = fNameGroups.group(1)
-                                    version = fNameGroups.group(2)
-                                    system = fNameGroups.group(3)
-                                    arch = fNameGroups.group(4)
-                                    armCompilation = fNameGroups.group(6)
-                                    extension = fNameGroups.group(7)
-                                    if armCompilation:
-                                        if armCompilation == "hflt":
-                                            arch = arch+"-h"
+                                if f_name_groups:
+                                    _type = f_name_groups.group(1)
+                                    _version = f_name_groups.group(2)
+                                    _system = f_name_groups.group(3)
+                                    _arch = f_name_groups.group(4)
+                                    arm_compilation = f_name_groups.group(6)
+                                    extension = f_name_groups.group(7)
+                                    if arm_compilation:
+                                        if arm_compilation == "hflt":
+                                            _arch = _arch+"-h"
                                         else:
-                                            arch = arch+"-s"
+                                            _arch = _arch+"-s"
                             i += 1
                         
-                        if dictType == "title":
-                            title = downloadLine.split(" = ")[1]
-                            title = title.replace('\"', '').replace(';','')
-                        elif dictType == "files":
-                            fileDict = downloadLine.split(" = ")[1];
+                        if dict_type == "title":
+                            title = download_line.split(" = ")[1]
+                            title = title.replace('\"', '').replace(';', '')
+                        elif dict_type == "files":
+                            file_dict_elements = download_line.split(" = ")
+                            if len(file_dict_elements) != 2:
+                                file_dict = None
+                            else:
+                                file_dict = file_dict_elements[1]
                         
-                        if fileDict and "new Array()" not in fileDict :
-                            dictCode = "dFileDict = "+fileDict
-                            exec dictCode
+                        if file_dict and "new Array()" not in file_dict:
+                            dict_code = "file_dict = " + file_dict
+                            exec dict_code
                             
-                            retitledDict = {}
+                            retitled_dict = {}
                             
-                            for k,v in dFileDict.items():
+                            for k, v in file_dict.items():
                                 if k == "title":
-                                    retitledDict[k] = title + " for " + v
+                                    retitled_dict[k] = title + " for " + v
                                 else:
-                                    retitledDict[k] = v
+                                    retitled_dict[k] = v
                             
-                            self.__versionsTree__[type][system][arch][version][extension] = retitledDict
+                            self.__versionsTree__[_type][_system][_arch][_version][extension] = retitled_dict
     
-    def getVersionsDict(self):
+    def get_versions_dict(self):
         return self.__versionsTree__
+
 
 def download(url, destination):
     file_name = url.split('/')[-1]
@@ -126,67 +125,71 @@ def download(url, destination):
     opener.addheaders.append(('Cookie', 'oraclelicense=accept-securebackup-cookie'))
     u = opener.open(url)
     
-    f = open(os.path.join(destination,file_name), 'wb')
+    f = open(os.path.join(destination, file_name), 'wb')
     meta = u.info()
-    file_size = float(meta.getheaders("Content-Length")[0])
+    file_size = int(meta.getheaders("Content-Length")[0])
     unit = "Bytes"
     size = file_size
-    if(file_size >= 1024 and file_size < 1024*1024):
+    if 1024*1024 > file_size >= 1024:
         unit = "KB"
         size = file_size/1024
-    elif(file_size >= 1024*1024):
+    elif file_size >= 1024*1024:
         unit = "MB"
         size = file_size/1024/1024
         
-    print "Downloading: %s %.2f %s" % (file_name, size,unit)
+    print "Downloading: %s %.2f %s" % (file_name, size, unit)
     
     file_size_dl = 0
     block_sz = 8192
     while True:
-        buffer = u.read(block_sz)
-        if not buffer:
+        _buffer = u.read(block_sz)
+        if not _buffer:
             break    
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        if(file_size_dl >= 1024 and file_size_dl < 1024*1024):
-            size = file_size_dl/1024
-            status = r"Downloaded: %dictGroup kB  [%3.2f%%]" % (size, file_size_dl * 100. / file_size)
+        file_size_dl += len(_buffer)
+        f.write(_buffer)
+        if 1024*1024 > file_size_dl >= 1024:
+            size = file_size_dl / 1024
+            status = r"Downloaded: %d kB  [%3.2f%%]" % (size, file_size_dl * 100. / file_size)
         
-        elif(file_size_dl >= 1024*1024):
+        elif file_size_dl >= 1024*1024:
             size = float(file_size_dl/1024.0/1024.0)
             status = r"Downloaded: %.2f MB  [%3.2f%%]" % (size, file_size_dl * 100. / file_size)
         else:
             status = r"Downloaded: %10d Bytes  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
         status = status + chr(8)*(len(status)+1)
-        print status,
+        print(status)
     f.close()
-    return os.path.join(destination,file_name)
+    return os.path.join(destination, file_name)
 
-def extractFile(fileName, destination):
+
+def extract_file(file_name, destination):
     
-    dirName = destination
+    dir_name = destination
     try:
-        tarFile = os.path.join(destination,fileName) 
-        tar = tarfile.open(tarFile, "r:gz")
+        tar_file = os.path.join(destination, file_name)
+        tar = tarfile.open(tar_file, "r:gz")
         tar.extractall(destination)
-        dirName = tar.getnames()[0]
+        dir_name = tar.getnames()[0]
         tar.close()
         time.sleep(1)
-        print "Removing file %s"%(tarFile)
-        os.remove(tarFile)
-    except tarfile.ReadError as e:
-        print e
-    return dirName
+        print "Removing file %s" % tar_file
+        os.remove(tar_file)
+    except tarfile.ReadError as _e:
+        print _e
+    return dir_name
+
 
 def is_writable(name):
     return os.access(name, os.W_OK)
 
-def shellcmd(cmd):
-    print cmd
+
+def shell_cmd(cmd):
+    print(cmd)
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    (o,e) = process.communicate()
-    print o,e
-    return (process.returncode, o, e)
+    (_o, _e) = process.communicate()
+    print _o, _e
+    return process.returncode, _o, _e
+
 
 def usage():
     print('\nUsage: java-updater [options]\nAvailable options:')
@@ -195,44 +198,47 @@ def usage():
     print("-f, --format\t\tFormat: tar.gz, tar, rpm, exe, bin, dmg, sh, tar.Z. Default: tar.gz")
     print("-t, --tool\t\tJava Environment: jdk, jre. Default: jdk")
     print("-d, --javadir\t\tJAVA directory in system. Default: /opt/java/\n")
-    print("-n, --newest\t\tInstall newest version of Java found on Oracle's pages. Default: False\n")
+    print("-n, --newest\t\tInstall newest version of Java found on Oracle's pages. Default: True\n")
 
 
-def chooseJavaType():
-    javaType = "jdk"
+def choose_java_type():
+    java_type = "jdk"
     print "Choose type of Java Environment: jdk, jre"
-    option = raw_input("[%s] >> "%javaType)
+    option = raw_input("[%s] >> " % java_type)
     if option.strip() == "jre":
-        javaType = "jre"
-    return javaType        
+        java_type = "jre"
+    return java_type
 
-def chooseOS():
-    os = "linux"
+
+def choose_os():
+    _os = "linux"
     print "Choose operating system:"
-    option = raw_input("[%s] >> "%os)
+    option = raw_input("[%s] >> " % _os)
     if option.strip() != "":
-        os = option.strip()
-    return os
+        _os = option.strip()
+    return _os
 
-def chooseArch():
-    arch = "x64"
+
+def choose_arch():
+    _arch = "x64"
     print "Choose system architecture"
-    option = raw_input("[%s] >> "%arch)
+    option = raw_input("[%s] >> " % _arch)
     if option.strip() != "":
-        arch = option.strip()
-    return arch
+        _arch = option.strip()
+    return _arch
 
-def chooseVersion(versions):
-    version = ""
+
+def choose_version(_versions):
+    _version = ""
     print "Choose one of the following Java versions:"
-    print " ".join(versions)
+    print " ".join(_versions)
     option = raw_input(">> ")
     if option.strip() != "":
-        version = option.strip()
-    return version
+        _version = option.strip()
+    return _version
 
 
-def chooseExtension(extensions):
+def choose_extension(extensions):
     extension = ""
     print "Choose java package file format: "
     print " ".join(extensions)
@@ -241,21 +247,22 @@ def chooseExtension(extensions):
         extension = option.strip()
     return extension
 
+
 def cmp_jversion(a, b):
     
-    aParts = a.split("u")
-    bParts = b.split("u")
+    a_parts = a.split("u")
+    b_parts = b.split("u")
     
-    a1 = aParts[0].replace("-p", "")
-    b1 = bParts[0].replace("-p", "")
+    a1 = a_parts[0].replace("-p", "")
+    b1 = b_parts[0].replace("-p", "")
     
     a2 = 0
     b2 = 0
     
-    if len(aParts) > 1:
-        a2 = aParts[1].replace("-p", "")
-    if len(bParts) > 1:
-        b2 = bParts[1].replace("-p", "")
+    if len(a_parts) > 1:
+        a2 = a_parts[1].replace("-p", "")
+    if len(b_parts) > 1:
+        b2 = b_parts[1].replace("-p", "")
     
     if int(a1) > int(b1):
         return 1
@@ -266,7 +273,6 @@ def cmp_jversion(a, b):
             return 0
         else:
             return -1
-        return 0
     else:
         return -1
 
@@ -277,7 +283,7 @@ if __name__ == "__main__":
     iFileFormat = "tar.gz"
     iJavaTool = "jdk"
     iJavaDir = "/opt/java"
-    iInstallNewest = False
+    iInstallNewest = True
     appDir = os.path.dirname(os.path.realpath(__file__))
         
     try:
@@ -307,15 +313,14 @@ if __name__ == "__main__":
         sys.exit(1)
 
     treeDict = {}
-    parser = DownloadLinksParser(dPages)
+    parser = DownloadLinksParser(download_pages)
     parser.parse()
-    treeDict = parser.getVersionsDict()              
+    treeDict = parser.get_versions_dict()
     
     javaType = iJavaTool
     if (javaType is None) or (javaType.strip() == ""):
-        javaType = chooseJavaType()
-    
-    
+        javaType = choose_java_type()
+
     typeTree = treeDict.get(javaType)
     if typeTree is None:
         print "Provided Java Environment: %s not found!" % javaType
@@ -323,9 +328,8 @@ if __name__ == "__main__":
     
     system = iSystem
     if (system is None) or (system.strip() == ""):
-        system = chooseOS()
+        system = choose_os()
 
-    
     systemTree = typeTree.get(system)
     if systemTree is None:
         print "Java for provided system: %s not found!" % system
@@ -333,62 +337,62 @@ if __name__ == "__main__":
     
     arch = iArch
     if (arch is None) or (arch.strip() == ""):
-        arch = chooseArch()
+        arch = choose_arch()
     
     archTree = systemTree.get(arch)
     if archTree is None:
         print "Java for provided architecture: %s and system: %s" % (arch, system)
         sys.exit(4)
-    
-    
+
     versions = archTree.keys()
     versions.sort(cmp_jversion, reverse=True)
     
     version = versions[0]
     if not iInstallNewest:
-        version = chooseVersion(versions)
+        version = choose_version(versions)
     
     versionTree = archTree.get(version)
     
     if versionTree is None:
-        print "Download link for Java: %s not found!" % (version)
+        print "Download link for Java: %s not found!" % version
         sys.exit(5)
     
     fileFormat = iFileFormat
     if (fileFormat is None) or (fileFormat.strip() == ""):
-        fileFormat = chooseExtension(versionTree.keys())
+        fileFormat = choose_extension(versionTree.keys())
     
     downloadFileDict = versionTree.get(fileFormat)
     if downloadFileDict is None:
         print "Can't find download link for following parameters:"
-        print "Java Environment: %s" % (javaType)
-        print "OS: %s" % (system)
-        print "Arch: %s" % (arch)
-        print "Version: %s" % (version)
-        print "File type: %s" % (fileFormat)
+        print "Java Environment: %s" % javaType
+        print "OS: %s" % system
+        print "Arch: %s" % arch
+        print "Version: %s" % version
+        print "File type: %s" % fileFormat
         sys.exit(6)
     
-    if downloadFileDict.has_key("filepath"):
+    if "filepath" in downloadFileDict.keys():
         print "Downloading file from link: %s Size: %s" % (downloadFileDict["filepath"], downloadFileDict["size"])
         downloadedFile = download(downloadFileDict["filepath"], iJavaDir)
-        print "Extracting file %s" % (downloadedFile)
+        print "Extracting file %s" % downloadedFile
         if fileFormat == "tar.gz":
             f_ext = downloadedFile.split(".")
             if f_ext[-1] == "gz":
-                main_dir = extractFile(downloadedFile, iJavaDir)
+                main_dir = extract_file(downloadedFile, iJavaDir)
             elif f_ext[-1] == "bin":
-                shellcmd("chmod +x %s"%(downloadedFile))
-                (rc, o, e) = shellcmd("cd %s; %s"%(iJavaDir,downloadedFile))
+                shell_cmd("chmod +x %s" % downloadedFile)
+                (rc, o, e) = shell_cmd("cd %s; %s" % (iJavaDir, downloadedFile))
                 o_lines = o.split("\n")
                 for line in o_lines:
                     l_elem = line.split(":")
                     if l_elem[0].strip() == "creating":
-                        main_dir = l_elem[1].replace("/","").strip()
-                        break;
-                shellcmd("rm %s"%(downloadedFile))
+                        main_dir = l_elem[1].replace("/", "").strip()
+                        break
+                shell_cmd("rm %s" % downloadedFile)
             else:
                 print "Unknown file format of downloaded file!"
                 sys.exit(6)
         else:
-            print "Unsupported installation for %s packages yet! Please install manually file: %s" % (fileFormat, downloadedFile)
+            print "Unsupported installation for %s packages yet! Please install manually file: %s" % (fileFormat,
+                                                                                                      downloadedFile)
             sys.exit(7)
